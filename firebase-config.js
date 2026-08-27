24f2a7bf01a5b6fa4c5ede330ab11f5e468e376f
ankitroy51jm <ankitroy51jm@gmail.com>
2026-08-27 16:40:15 +0530

    Create firebase-config.js
    H

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD3k4A8cOkFXtdxMTjevs8mQstTEHw80TY",
+  authDomain: "dj-karni-kripa.firebaseapp.com",
+  databaseURL: "https://dj-karni-kripa-default-rtdb.firebaseio.com",
+  projectId: "dj-karni-kripa",
+  storageBucket: "dj-karni-kripa.firebasestorage.app",
+  messagingSenderId: "724610528893",
+  appId: "1:724610528893:web:03f01f2f201f86e73da314",
+  measurementId: "G-LQYG46DY95"
+};
