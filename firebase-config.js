e2e1cf20f840b3ad0a4cee98157a199519f7b05e
ankitroy51jm <ankitroy51jm@gmail.com>
2026-08-27 17:07:42 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,20 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD3k4A8cOkFXtdxMTjevs8mQstTEHw80TY",
-  authDomain: "dj-karni-kripa.firebaseapp.com",
-  databaseURL: "https://dj-karni-kripa-default-rtdb.firebaseio.com",
-  projectId: "dj-karni-kripa",
-  storageBucket: "dj-karni-kripa.firebasestorage.app",
-  messagingSenderId: "724610528893",
-  appId: "1:724610528893:web:03f01f2f201f86e73da314",
-  measurementId: "G-LQYG46DY95"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGM7L7lQDBebku5s6QY7TnB10tVKibmWE",
+    authDomain: "dj-karni-kripa-booking.firebaseapp.com",
+    databaseURL: "https://dj-karni-kripa-booking-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "dj-karni-kripa-booking",
+    storageBucket: "dj-karni-kripa-booking.firebasestorage.app",
+    messagingSenderId: "732937812471",
+    appId: "1:732937812471:web:a647780175ea3c396956f2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
