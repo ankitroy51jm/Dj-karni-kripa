f47a442f297abfde8722b39ad63f0d9030ac2b40
ankitroy51jm <ankitroy51jm@gmail.com>
2026-08-27 13:42:43 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyD3k4A8cOkFXtdxMTjevs8mQstTEHw80TY",
+  authDomain: "dj-karni-kripa.firebaseapp.com",
+  projectId: "dj-karni-kripa",
+  storageBucket: "dj-karni-kripa.firebasestorage.app",
+  messagingSenderId: "724610528893",
+  appId: "1:724610528893:web:03f01f2f201f86e73da314",
+  measurementId: "G-LQYG46DY95"
+};
