88991731dc1594d9fb6d4f86ac937c63aa518740
ankitroy51jm <ankitroy51jm@gmail.com>
2026-08-27 17:22:03 +0530

    Update firebase-config.js
    Change

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+window.DJ_FIREBASE_CONFIG = {
   apiKey: "AIzaSyD3k4A8c0fXJds3MfjvsdqgStEHw80TY",
   authDomain: "dj-karni-kripa.firebaseapp.com",
   databaseURL: "https://dj-karni-kripa-default-rtdb.firebaseio.com",
@@ -8,5 +8,3 @@ const firebaseConfig = {
   appId: "1:724610528893:web:03f01f201f86e73da314",
   measurementId: "G-LQYG46DY95"
 };
-
-firebase.initializeApp(firebaseConfig);
